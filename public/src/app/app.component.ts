d2f995ebd957539594eab188c6988d11d444f2e0
unknown <Administrateur@161-UC56-09.ad.campus-eni.fr>
2018-01-09 16:44:07 +0100

    project clarity

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import 'clarity-icons';
+import 'clarity-icons/shapes/all-shapes';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
